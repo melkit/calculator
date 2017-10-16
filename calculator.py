0a3b1133d238681bce8281cf2063f38ea2b7ad61
melkit <31418776+melkit@users.noreply.github.com>
2017-10-16 13:51:07 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,31 @@
+print ("***** ADDITION*****")
+number1 =(int)(input("Enter your first number: "))
+number2 =(int)(input("Enter your second number: "))
+
+answer = number1+number2
+
+print ("The answer = " + str(answer))
+
+print ("***** SUBSTRACTION *****")
+number1 =(int)(input("Enter your first number: "))
+number2 =(int)(input("Enter your second number: "))
+
+answer = number1-number2
+
+print ("The answer = " + str(answer))
+
+print ("***** MULTIPLICATION *****")
+number1 =(int)(input("Enter your first number: "))
+number2 =(int)(input("Enter your second number: "))
+
+answer = number1*number2
+
+print ("The answer = " + str(answer))
+
+print ("***** DIVISION *****")
+number1 =(int)(input("Enter your first number: "))
+number2 =(int)(input("Enter your second number: "))
+
+answer = number1/number2
+
+print ("The answer = " + str(answer))
